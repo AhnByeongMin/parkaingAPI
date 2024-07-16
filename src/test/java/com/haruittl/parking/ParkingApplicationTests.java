65d4976e6d9a8d4cbff1de3ac6f3b7fdf5cd17d3
haruHome <qudals0617@naver.com>
2024-07-16 15:45:25 +0900

    parking API용 프로젝트 생성

---
+++ src/test/java/com/haruittl/parking/ParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haruittl.parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
